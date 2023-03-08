04ce96fd590b6a5e184aa183afa418c74427da7a
Slavouz Corp <tmbwpva@protonmail.com>
2023-03-08 19:40:43 +0700

    second commit

---
+++ src/modul1/latihan1/Makanan.java
@@ -0,0 +1,11 @@
+package modul1.latihan1;
+
+/**
+ *
+ * @author davinky
+ */
+public class Makanan extends Hidangan{
+    public String disantap() {
+        return this.getNamaHidangan() + " dimakan";
+    }
+}
---
+++ src/modul1/latihan1/Minuman.java
@@ -0,0 +1,11 @@
+package modul1.latihan1;
+
+/**
+ *
+ * @author davinky
+ */
+public class Minuman extends Hidangan {
+    public String disantap() {
+        return this.getNamaHidangan() + " diminum";
+    }
+}
